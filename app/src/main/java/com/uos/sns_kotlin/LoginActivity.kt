d1c0c3b684434e2242da57d08661188cff0dfbbb
4538cgy <58514224+4538cgy@users.noreply.github.com>
2020-09-06 17:31:19 +0900

    commit facebook login

--- app/src/main/java/com/uos/sns_kotlin/LoginActivity.kt
+++ app/src/main/java/com/uos/sns_kotlin/LoginActivity.kt
@@ -1,29 +1,56 @@
 package com.uos.sns_kotlin
 
 import android.content.Intent
+import android.content.pm.PackageManager
+
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthCredential
+
 import com.google.firebase.auth.GoogleAuthProvider
+
 import kotlinx.android.synthetic.main.activity_login.*
+import java.security.MessageDigest
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     var auth : FirebaseAuth? = null
     var googleSignInClient : GoogleSignInClient ? = null
     var GOOGLE_LOGIN_CODE = 9001
+    var callbackManager : CallbackManager? = null
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+
+
+
+
 
         auth  = FirebaseAuth.getInstance()
         email_login_button.setOnClickListener {
@@ -35,6 +62,12 @@ class LoginActivity : AppCompatActivity() {
             googleLogin()
         }
 
+        facebook_login_button.setOnClickListener {
+            //first step
+            facebookLogin()
+
+        }
+
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
@@ -42,7 +75,7 @@ class LoginActivity : AppCompatActivity() {
 
         googleSignInClient = GoogleSignIn.getClient(this,gso)
 
-
+        callbackManager = CallbackManager.Factory.create()
 
     }
     fun googleLogin(){
@@ -50,8 +83,52 @@ class LoginActivity : AppCompatActivity() {
         startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE)
     }
 
+    fun facebookLogin(){
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile","email"))
+
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
+                override fun onSuccess(result: LoginResult?) {
+
+                    //second step
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+
+            })
+    }
+
+    fun handleFacebookAccessToken(token : AccessToken?){
+
+        var credential  = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)?.addOnCompleteListener {
+                task ->
+            if(task.isSuccessful){
+
+                //third step
+                //login
+                moveMainPage(task.result?.user)
+            }else{
+                //show the error message
+                Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager?.onActivityResult(requestCode,resultCode,data)
+
         if(requestCode == GOOGLE_LOGIN_CODE){
             var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if (result != null) {
